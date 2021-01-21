18a9c472eb37f950cb0561d2bc062159047cfca8
Melanie Buck <melaniebuck93@gmail.com>
2021-01-21 17:34:53 +0100

    GET for iteration 4 does not work

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -25,6 +25,60 @@ router.get('/celebrities/:id', (req, res, next) => {
     })
 });
 
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/create');
+});
+
+router.post('/celebrities', (req, res, next) => {
+    const data = req.body;
+
+    Celebrity.create({
+        name: data.name,
+        occupation: data.occupation,
+        catchPhrase: data.catchPhrase
+    }).then((celebrities) => {
+        res.redirect('/');
+    }).catch(error => {
+        next('error');
+    })
+})
+
+router.post('/celebrities/:id/delete', (req, res, next) => {
+    const id = req.params.id;
+    Celebrity.findByIdAndRemove(id)
+    .then((celebrities) => {
+        res.redirect('/');
+    })
+    .catch(error => {
+        next('error');
+    })
+});
+
+
+router.get('/celebrities/:id/edit', (req, res, next) => {
+    const id = req.params.id;
+    Celebrity.findbyId(id)
+    .then((celebrities) => {
+        res.render('celebrities/edit', { celebrities : celebrities } );
+    })
+    .catch(error => {
+        next('error');
+    })
+});
+
+router.post('/celebrities/:id/edit', (req, res, next) => {
+    const id = req.params.id;
+    const data = req.body;
+    Celebrity.findByIdAndUpdate({
+        name: data.name,
+        occupation: data.occupation,
+        catchPhrase: data.catchPhrase
+    }).then((celebrities) => {
+        res.redirect(`/celebrities/${celebrity._id}`);
+    }).catch(error => {
+        next('error');
+    })
+})
 
 
 module.exports = router;
